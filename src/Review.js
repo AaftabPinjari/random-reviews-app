e5de26fc2fb39ff022dc63b4cf49b26d06ddb4e2
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-03-26 14:13:48 +0530

    a bug in random review buttoneverything else done

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,62 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  const [index,setIndex]=useState(0)
+  const {name,text,job,image}=people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length-1){
+      return 0;
+    }
+    if (number < 0){
+      return people.length -1;
+    }
+    return number;
+  }
+  const nextPerson = () => {
+    setIndex((index)=>{
+      let newIndex = index +1;
+      return checkNumber(newIndex)
+    });
+  };
+  const prevPerson = () => {
+    setIndex((index)=>{
+      let newIndex = index -1;
+      return checkNumber(newIndex)
+    });
+  };
+  const randomPerson = ()=>{
+    let randomNumber = Math.floor(Math.random * people.length);
+    if(randomNumber === index){
+      randomNumber= index + 1;
+      console.log(randomNumber);
+    }
+    setIndex(()=>{return checkNumber(randomNumber)});
+  };
+  return (
+    <article className="review">
+      <div className="img-container">
+      <img src={image} alt="name" className="person-img"/>
+      <span className="qoute-icon"><FaQuoteRight /></span>
+      </div>
+      <div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+      </div>
+      <div className="button-container">
+        <button className="prev-btn" onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+        </div>
+        <button className="random-btn" onClick={randomPerson}>
+        Random Review
+        </button>
+
+    </article>
+  );
+}
 export default Review;
